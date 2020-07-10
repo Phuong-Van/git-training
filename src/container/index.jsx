19e53013c2eb3362309f8c348ca6d33d5e77f06d
INI\anh.nguyen <anh.nguyen@terralogic.com>
2020-07-10 09:26:05 +0700

    add container

---
+++ src/container/index.jsx
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class index extends PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>Anh-Nguyen dep trai =]]]</h1>
+            </div>
+        );
+    }
+}
+
+index.propTypes = {
+
+};
+
+export default index;
